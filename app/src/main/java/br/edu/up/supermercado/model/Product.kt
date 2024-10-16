a3019947c546bacb772c760fa468969030d319e8
Auto Logon <autologon@CSED.LAB>
2024-10-15 21:13:55 -0300

    Added CarItem and Product at model

---
+++ app/src/main/java/br/edu/up/supermercado/model/Product.kt
@@ -0,0 +1,12 @@
+package br.edu.up.supermercado.model
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "products")
+data class Product(
+    @PrimaryKey(autoGenerate = true) val id: Long = 0,
+    val name: String,
+    val price: Double,
+    val description: String
+)
